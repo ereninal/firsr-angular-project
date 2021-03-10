7a3bad80e67947ca61715389fe20e76c39902620
Eren İNAL <ereninal10@gmail.com>
2021-03-10 23:46:12 +0300

    fixed

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-product',
@@ -7,18 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product = { productId: 1, productName: "Laptop", categoryId: 2, unitPrice: 200 }
-  product2 = { productId: 2, productName: "Klavye", categoryId: 2, unitPrice: 200 }
-  produc3 = { productId: 3, productName: "Masaüstü Pc", categoryId: 2, unitPrice: 200 }
-  produc4 = { productId: 4, productName: "Mouse", categoryId: 2, unitPrice: 200 }
-  produc5 = { productId: 5, productName: "Ekran", categoryId: 2, unitPrice: 200 }
-  produc6 = { productId: 6, productName: "İşlemci", categoryId: 2, unitPrice: 200 }
-  produc7 = { productId: 7, productName: "Router", categoryId: 2, unitPrice: 200 }
-
-  products = [this.product2, this.produc3, this.produc4, this.produc5, this.produc6, this.produc7]
-  constructor() { }
+  
+  products:Product[] = [];
+  apiUrl = "https://jsonplaceholder.typicode.com/todos";
+  
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    console.log("Response: "+ this.products)
   }
+  getProducts() {
 
+    this.httpClient
+        .get<Product>(this.apiUrl)
+        .subscribe((response) =>{
+          this.products = [response]
+          
+        })
+  }
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,6 @@
+export interface Product{
+    userId:number;
+    id:number;
+    title:string;
+    completed:boolean
+}
